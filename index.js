c3a5650d1e81658be3452ff64cb75d071591ddfb
Akash1Verma2 <ankitverma8621@gmail.com>
2026-03-16 19:10:28 +0530

    Updated the code

--- index.js
+++ index.js
@@ -6,28 +6,49 @@ const path = require("path");
 app.use(express.urlencoded({extended: true}));
 
 app.set("view engine", "ejs");
-app.set("view", path.join(__dirname, "views"));
+app.set("views", path.join(__dirname, "views"));
 
-app.set(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "public")));
 
 let posts = [
     {
+        id:"1a",
         username: "apnacollege",
         content: "I love coding"
     },
     {
+        id:"2b",
         username: "AkashVerma",
-        content: "hard work is impoert to achieve success"
+        content: "Hard work is important to achieve success"
     },
     {
+        id:"3c",
         username: "MohitVerma",
         content: "I got selected for my 1st intership"
     },
 
 ]
 
-app.get("/", (req, res) => {
-    res.send("serving working well!");
+app.get("/posts", (req, res) => {
+    res.render("index.ejs", {posts});
+})
+
+app.get("/posts/new", (req, res) => {
+    res.render("new.ejs");
+
+})
+
+app.post("/posts", (req, res) => {
+    let { username, content } = req.body;
+    posts.push({username, content});
+    res.redirect("/posts");
+})
+
+app.get("/posts/:id", (req, res) => {
+    let {id} = req.params;
+    let post = posts.find((p) => id === p.id);
+    res.render("show.ejs", {post});
+
 })
 
 app.listen(port, () => {
